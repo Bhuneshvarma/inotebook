0a40ba5cc0e7e4a599fece62eade90544485d97c
Bhunesh varma <bhuneshvarma63@gmail.com>
2024-05-08 14:29:20 +0530

    new changes

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -9,7 +9,7 @@ const Login = ({ showAlert }) => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch(`http://localhost:5000/api/auth/login`, {
+            const response = await fetch(`http://localhost/api/auth/login`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
