8f4f0be8d750ed33f4129764005a5598dfb35a1b
Bhunesh varma <87127362+Bhuneshvarma@users.noreply.github.com>
2024-05-11 18:14:16 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,59 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { createProxy } from 'vite-plugin-middleware';
 
 export default defineConfig({
-  plugins: [react()],
-  base: "inotebook"
+  server: {
+    proxy: createProxy({
+      '/api': {
+        target: 'https://inotebook-backend-rust.vercel.app/',
+        changeOrigin: true,
+        secure: false,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+        },
+      },
+      '/api/auth/createuser': {
+        target: 'https://inotebook-backend-rust.vercel.app/',
+        changeOrigin: true,
+        secure: false,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'POST',
+          'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+        },
+      },
+      '/api/auth/login': {
+        target: 'https://inotebook-backend-rust.vercel.app/',
+        changeOrigin: true,
+        secure: false,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'POST',
+          'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+        },
+      },
+      '/api/auth/getuser': {
+        target: 'https://inotebook-backend-rust.vercel.app/',
+        changeOrigin: true,
+        secure: false,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'POST',
+          'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+        },
+      },
+      '/api/auth/updateuser': {
+        target: 'https://inotebook-backend-rust.vercel.app/',
+        changeOrigin: true,
+        secure: false,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'PUT',
+          'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+        },
+      },
+    }),
+  },
 });
